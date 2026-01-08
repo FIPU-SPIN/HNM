ef35a327da4ff66f1747de6afc194c1ec7730595
Mia Rovis <mrovis@student.unipu.hr>
2026-01-08 12:01:29 +0100

    Page 2 update

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const burger = document.querySelector(".burger");
-    const nav = document.querySelector(".main-nav ul");
 
-    if (burger && nav) {
-        burger.addEventListener("click", () => {
-            nav.classList.toggle("active");
-        });
-    }
-});
