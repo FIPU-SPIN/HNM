0e55281afae39c045452efea2c68a8d67ad5db6c
Mia Rovis <mrovis@student.unipu.hr>
2026-01-08 10:16:13 +0100

    Što sve znam
    Izrada nove stranice

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Uspješno pokrenuto.");
+document.addEventListener("DOMContentLoaded", () => {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".main-nav ul");
+
+    if (burger && nav) {
+        burger.addEventListener("click", () => {
+            nav.classList.toggle("active");
+        });
+    }
+});
